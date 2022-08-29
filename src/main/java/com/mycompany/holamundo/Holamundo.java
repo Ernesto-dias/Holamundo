e3751f32fa48e900eaf59d9906eb29ef1efaa66c
Ernesto-dias <ambrosiomarmol@gmail.com>
2022-08-29 22:31:04 +0200

    primer commit

---
+++ src/main/java/com/mycompany/holamundo/Holamundo.java
@@ -0,0 +1,15 @@
+/*Licensias de uso */
+
+package com.mycompany.holamundo;
+
+/*Autor@ernestodiaz*/
+
+public class Holamundo { 
+public static void  main (String[] args) {
+    System.out.println("Holamundo"); //operaciones//
+    
+}
+    
+        
+}
+//Una funcion, en java llamada class esta compuesta por el nombre de la funcion y luego entre parentecis los tipos
